efd3050f675f4a58b73ecc38617eb175fc6f3776
Steren <steren.giannini@gmail.com>
2012-06-20 02:35:49 -0700

    new project using WinJS 1.0 RC

--- Template/Template/js/default.js
+++ Template/Template/js/default.js
@@ -4,27 +4,40 @@
     "use strict";
 
     var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
+    WinJS.strictProcessing();
 
-    app.onactivated = function (eventObject) {
-        if (eventObject.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
-            if (eventObject.detail.previousExecutionState !== Windows.ApplicationModel.Activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize 
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
                 // your application here.
             } else {
-                // TODO: This application has been reactivated from suspension. 
+                // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
             }
-            WinJS.UI.processAll();
-        }
-    };
 
-    app.oncheckpoint = function (eventObject) {
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
+        }
+    });
+
+    app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the 
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // eventObject.setPromise(). 
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
     };
 
     app.start();
