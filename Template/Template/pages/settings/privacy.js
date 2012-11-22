5c6a7e1d43a8c72185cc2b4fe13f378e41583ba4
Steren <steren.giannini@gmail.com>
2012-11-22 16:14:56 +0100

    custom privacy, about and support messages

---
+++ Template/Template/pages/settings/privacy.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    var page = WinJS.UI.Pages.define("/pages/settings/privacy.html", {
+
+        ready: function (element, options) {
+            var aboutElement = document.getElementById('js-privacy-content');
+            aboutElement.innerHTML = Joshfire.factory.config.template.options.privacyhtml;
+        }
+
+    });
+
+})();
---
+++ Template/Template/pages/settings/support.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    var page = WinJS.UI.Pages.define("/pages/settings/support.html", {
+
+        ready: function (element, options) {
+            var aboutElement = document.getElementById('js-support-content');
+            aboutElement.innerHTML = Joshfire.factory.config.template.options.supporthtml;
+        }
+
+    });
+
+})();
